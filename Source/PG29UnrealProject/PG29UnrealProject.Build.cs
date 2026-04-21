70846981e644f53715270049c2e20db9c7b42694
squidypal <78044718+squidypal@users.noreply.github.com>
2026-04-21 16:21:22 -0700

    Squashed commit of the following:
    commit 23a89ea13f57d60948df7258f2e708df9ed175c8
    Author: squidypal <squidypal@outlook.com>
    Date:   Mon Apr 20 20:54:00 2026 -0700
    
        Lobby Fix
    
    commit 6c8e0d4c1ff0147775d4a4312da47bc4dfc9d846
    Author: squidypal <squidypal@outlook.com>
    Date:   Mon Apr 20 20:46:05 2026 -0700
    
        UI Setup

--- Source/PG29UnrealProject/PG29UnrealProject.Build.cs
+++ Source/PG29UnrealProject/PG29UnrealProject.Build.cs
@@ -13,16 +13,13 @@ public class PG29UnrealProject : ModuleRules
 			"Engine",
 			"InputCore",
 			"EnhancedInput",
-			"GameplayTags"});
+			"GameplayTags",
+			"UMG",
+			"Slate",
+			"SlateCore",
+			"OnlineSubsystem",
+			"OnlineSubsystemUtils"});
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
-
-		// Uncomment if you are using Slate UI
-		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
-		
-		// Uncomment if you are using online features
-		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
-
-		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
 	}
 }
---
+++ Source/PG29UnrealProject/Public/LobbyGameMode.h
@@ -0,0 +1,36 @@
+// Copyright VFS 2026 - Rohit
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameMode.h"
+#include "LobbyGameMode.generated.h"
+
+DECLARE_LOG_CATEGORY_EXTERN(LogLobbyGameMode, Log, All);
+
+UCLASS()
+class PG29UNREALPROJECT_API ALobbyGameMode : public AGameMode
+{
+	GENERATED_BODY()
+
+public:
+	ALobbyGameMode();
+
+	virtual void PostLogin(APlayerController* NewPlayer) override;
+	virtual void Logout(AController* Exiting) override;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Lobby")
+	FString GameMapName;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Lobby")
+	int32 MinPlayersToStart = 2;
+
+	UFUNCTION(BlueprintCallable, Category="Lobby")
+	void RequestStartMatch();
+
+	UFUNCTION(BlueprintPure, Category="Lobby")
+	int32 GetConnectedPlayerCount() const;
+
+protected:
+	int32 ConnectedPlayers = 0;
+};
